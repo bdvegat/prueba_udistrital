75b017998d9b9a2f443e9e6937f18e08448d920c
Bryan Vega <bdvegat@unal.edu.co>
2021-06-12 00:47:51 -0500

    team and match models

--- cuadrangular/models.py
+++ cuadrangular/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    team_name = models.CharField(max_length=30) 
+
+class Match(models.Model):
+    team1 = models.ForeignKey(Team,related_name='team1', on_delete=models.CASCADE)
+    team2 = models.ForeignKey(Team,related_name='team2', on_delete=models.CASCADE)
+    team1_score = models.IntegerField(default=0)
+    team2_score = models.IntegerField(default=0)
